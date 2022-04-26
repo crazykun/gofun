d4d8b03b63f7552e25f754b320b6b0eaf8b06a85
吉立坤 <jilikun@dofun.cn>
2022-04-26 19:25:30 +0800

    添加心跳接口

--- app/route/router.go
+++ app/route/router.go
@@ -2,11 +2,15 @@ package routes
 
 import (
 	"gofun/app/controller"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine) {
 	router.GET("/", controller.Index)
-
+	// router.GET("/test", controller.Test)
+	router.GET("/health-check", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome Gofun Server")
+	})
 }
