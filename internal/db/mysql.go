b5a9413d0ac54b6c7e3b3e8050911dd00da350b4
吉立坤 <jilikun@dofun.cn>
2022-04-26 17:21:00 +0800

    数据库链接失败后中断

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -3,6 +3,8 @@ package db
 import (
 	"fmt"
 	"gofun/conf"
+	"log"
+	"os"
 	"time"
 
 	"gorm.io/driver/mysql"
@@ -33,14 +35,17 @@ func RegisterMysqlPool(clientName string, dbConfig conf.MySQLConfig) {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database, dbConfig.Charset)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err == nil {
-		sqlDB, _ := db.DB()
-
-		sqlDB.SetMaxIdleConns(dbConfig.MinNum) // SetMaxIdleConns 设置空闲连接池中连接的最大数量
-		sqlDB.SetMaxOpenConns(dbConfig.MaxNum) // SetMaxOpenConns 设置打开数据库连接的最大数量。
-		sqlDB.SetConnMaxLifetime(time.Hour)    // SetConnMaxLifetime 设置了连接可复用的最大时间。
-		pool.addInstance(clientName, db)
+	if err != nil {
+		log.Println("数据库", clientName, "连接失败：", err)
+		os.Exit(200)
 	}
+
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxIdleConns(dbConfig.MinNum) // SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxOpenConns(dbConfig.MaxNum) // SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetConnMaxLifetime(time.Hour)    // SetConnMaxLifetime 设置了连接可复用的最大时间。
+	pool.addInstance(clientName, db)
+
 }
 
 func GetMysqlPool() *MysqlPool {
